d76f4ae2a6b42b0cd79bec3cb3fbb0a036a602ed
sweta <swetabisht4b@outlook.com>
2022-12-14 14:29:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+var readlineSync = require('readline-sync')
+
+var score = 0
+var userName = readlineSync.question("What's your name? ")
+
+console.log('Welcome ' + userName + ' to - DO YOU KNOW SWETA?')
+
+// data of high score
+var highScores = [
+  {
+    name: "SUMIT",
+    score: 5,
+  },
+  {
+    name: 'REENA',
+    score: 4,
+  },
+]
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question)
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log('Right! Seems like you really know Sweta.')
+    score = score + 1
+  } else {
+    console.log('Oops! the correct answer is ' + answer)
+  
+  }
+
+  console.log('Current Score: ', score)
+  console.log('-----------------')
+}
+
+// array of objects
+var questions = [
+  {
+    question: 'Where do I live? ',
+    answer: 'Pune',
+  },
+  {
+    question: 'Which is my favorite series? ',
+    answer: 'Sherlock holmes',
+  },
+  
+  {
+    question: 'Which is my favorire cartoon character? ',
+    answer: 'Doraemon',
+  },
+  
+  {
+    question: 'Which is my favorite drink? ',
+    answer: 'Cocacola',
+  },
+  {
+    question: "Here's the last question - Which is my favorite song? ",
+    answer: 'Ai Meri Zohrajabeen',
+  },
+]
+
+// loop
+for (var i = 0; i < questions.length; i++) {
+  play(questions[i].question, questions[i].answer)
+}
+
+console.log('YAY! You SCORED: ', score)
+console.log('-------------------')
+
+console.log('Check out the high scorers - ')
+for (var i = 0; i < highScores.length; i++) {
+  console.log(highScores[i].name + ': ' + highScores[i].score)
+}
+console.log('-------------------')
+
+console.log(
+  'If you have scored better than the current high scorers then send me a screenshot of your score, so that I can add you to the high scorers list.'
+)
+console.log('Thank you for playing!')
